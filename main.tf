dfdffcccf79ad3225d36bcb109dcf6572bdd0500
amit <amit@ow.com>
2024-04-21 17:13:42 +0000

    Added code

--- main.tf
+++ main.tf
@@ -6,4 +6,12 @@ key = "student.01-network-state"
 region = "us-east-1"
 }
 }
+resource "aws_instance" "my_vm" {
+ami = "ami-0cf43e890af9e3351"
+subnet_id = data.terraform_remote_state.network_details.outputs.my_subnet
+instance_type = "t3.micro"
+tags = {
+Name = "amitvm"
+}
+}
 
